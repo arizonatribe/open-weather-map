a2bdc8ac5e9fc27018860c5a6176b874ce324107
David Nunez <arizonatribe@gmail.com>
2024-02-20 12:00:51 -0700

    feat: created a file to handle the web server proxying calls to OpenWeatherMap API

---
+++ server.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Using a light dependency-injection pattern, modeled here:
+// - https://stackoverflow.com/a/60324692/2022208
+
+type WeatherController struct {
+	Service *ApiService
+}
+
+func (controller *WeatherController) GetCurrentWeatherByLocation(c *gin.Context) {
+	q := c.Request.URL.Query()
+
+	coords, err := ParseCoordinates(q.Get("lat"), q.Get("lon"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Lat/lon value is invalid", "error": err})
+		return
+	}
+
+	res, apiErr := controller.Service.GetCurrentWeather(coords)
+	if apiErr != nil {
+		c.String(apiErr.Code, apiErr.Message)
+		return
+	}
+
+	c.JSON(http.StatusOK, res.GetWeatherCondition())
+}
+
+func SetupRoutes(baseUrl string, apiKey string) *gin.Engine {
+	r := gin.New()
+	c := WeatherController{Service: NewApiService(baseUrl, apiKey)}
+
+	r.GET("/weather", c.GetCurrentWeatherByLocation)
+
+	return r
+}
---
+++ server_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_CurrentWeather_Route(t *testing.T) {
+	requestUrl := fmt.Sprintf(
+		"/weather?lat=%s&lon=%s",
+		strconv.FormatFloat(successMarshaledResponse.Coords.Lat, 'f', -1, 64),
+		strconv.FormatFloat(successMarshaledResponse.Coords.Lon, 'f', -1, 64),
+	)
+
+	successResponseServer := httptest.NewServer(http.HandlerFunc(handleSuccessResponse))
+	router := SetupRoutes(successResponseServer.URL, apiKey)
+	w := httptest.NewRecorder()
+
+	req, _ := http.NewRequest("GET", requestUrl, nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, `{"activity":["Rain"],"condition":"hot"}`, w.Body.String())
+}
